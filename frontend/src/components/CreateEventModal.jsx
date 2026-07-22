751280d796f4ff1f3a8b9e7d09d966cd00ed3a8d
unknown <thoutuakshaya1@gmail.com>
2026-07-22 16:55:08 +0530

    https://miniproject-1asj.onrender.com

--- frontend/src/components/CreateEventModal.jsx
+++ frontend/src/components/CreateEventModal.jsx
@@ -18,7 +18,7 @@ function CreateEventModal({ isOpen, onClose, onEventAdded }) {
     const token = localStorage.getItem("userToken");
 
     try {
-      await axios.post("https://miniproject-1-34zo.onrender.com/api/events", formData, {
+      await axios.post("https://miniproject-1asj.onrender.com", formData, {
         headers: { Authorization: `Bearer ${token}` }
       });
       onEventAdded(); // <-- call correct prop here to refresh events
